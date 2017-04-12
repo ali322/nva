27e7df0ee07524b1dfacee314ae63a919e3b63f5
alichen <ali322@gmail.com>
2017-04-12 16:46:46 +0800

    update 0.1.71

--- packages/nva-core/src/lib.js
+++ packages/nva-core/src/lib.js
@@ -1,7 +1,9 @@
 import HappyPack from 'happypack'
 import os from 'os'
+import autoPrefixer from 'autoprefixer'
+import sprites from 'postcss-sprites'
 
-export function happypackPlugin(id, loaders,tempDir) {
+export function happypackPlugin(id, loaders, tempDir) {
     const compilerThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
     return new HappyPack({
         id,
@@ -10,4 +12,48 @@ export function happypackPlugin(id, loaders,tempDir) {
         threadPool: compilerThreadPool,
         loaders
     })
+}
+
+export const postcssOptions = ({HOT,SPRITE_OUTPUT})=>({
+    plugins: function() {
+        let plugins = [
+            autoPrefixer()
+        ]
+        if (!HOT) {
+            plugins.push(sprites({
+                spritePath: SPRITE_OUTPUT
+            }))
+        }
+        return plugins
+    }
+})
+
+export function cssLoaders(constants, preprocessor = '', inline = false) {
+    const { SPRITE_OUTPUT } = constants
+
+    let cssLoaders = [
+        { loader: 'style-loader' },
+        { loader: 'css-loader', options: { minimize: !constants.HOT } },
+        { loader: 'postcss-loader', options: postcssOptions(constants) },
+        { loader: 'resolve-url-loader' }
+    ]
+
+    /* if inline mode,like in vue-loader */
+    if (inline) {
+        cssLoaders = cssLoaders.slice(0, -2).concat(cssLoaders.slice(-1))
+    }
+    if (preprocessor) {
+        if (!inline && typeof preprocessor === 'string') {
+            cssLoaders = [
+                ...cssLoaders,
+                { loader: 'happypack/loader', options: { id: preprocessor } }
+            ]
+        } else {
+            cssLoaders = [
+                ...cssLoaders,
+                typeof preprocessor === 'string' ? `${preprocessor}-loader` : preprocessor
+            ]
+        }
+    }
+    return cssLoaders
 }
--- packages/nva-core/src/webpack/loaders.js
+++ packages/nva-core/src/webpack/loaders.js
@@ -1,25 +1,11 @@
 import path from 'path'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import autoPrefixer from 'autoprefixer'
-import sprites from 'postcss-sprites'
+import { cssLoaders as cssLoadersFactory, postcssOptions } from '../lib'
 
 const nodeModulesDir = path.join(process.cwd(), 'node_modules')
 
 export default function(constants) {
-    const { ASSET_FONT_OUTPUT, ASSET_IMAGE_OUTPUT, SPRITE_OUTPUT, IMAGE_PREFIX, FONT_PREFIX } = constants
-    const postcssOptions = {
-        plugins: function() {
-            let plugins = [
-                autoPrefixer()
-            ]
-            if (!constants.HOT) {
-                plugins.push(sprites({
-                    spritePath: SPRITE_OUTPUT
-                }))
-            }
-            return plugins
-        }
-    }
+    const { ASSET_FONT_OUTPUT, ASSET_IMAGE_OUTPUT, IMAGE_PREFIX, FONT_PREFIX, HOT } = constants
     const fileLoaderOptions = {
         publicPath: function(url) {
             var _prefix = ''
@@ -32,50 +18,13 @@ export default function(constants) {
         }
     }
 
-    const cssLoaders = [
-        { loader: 'style-loader' },
-        { loader: 'css-loader', options: { minimize: !constants.HOT } },
-        { loader: 'postcss-loader', options: postcssOptions },
-        { loader: 'resolve-url-loader' }
-    ]
-    const lessLoaders = [...cssLoaders, { loader: 'happypack/loader', options: { id: "less" } }]
-    const sassLoaders = [...cssLoaders, { loader: 'happypack/loader', options: { id: "sass" } }]
-    const stylusLoaders = [...cssLoaders, { loader: 'happypack/loader', options: { id: "stylus" } }]
+    const cssLoaders = cssLoadersFactory(constants)
+    const lessLoaders = cssLoadersFactory(constants, 'less')
+    const sassLoaders = cssLoadersFactory(constants, 'sass')
+    const stylusLoaders = cssLoadersFactory(constants, 'stylus')
 
-    const vueLoaderOptions = constants.HOT ? {
-        loaders: {
-            css: [...cssLoaders.slice(0, -2),'resolve-url-loader'],
-            less: [...cssLoaders.slice(0, -2), 'less-loader'],
-            stylus: [...cssLoaders.slice(0, -2), 'stylus-loader']
-        }
-    } : {
-        loaders: {
-            css: ExtractTextPlugin.extract({
-                use: [{ loader: 'css-loader' }, { loader: "resolve-url-loader" }],
-                fallback: 'vue-style-loader'
-            }),
-            less: ExtractTextPlugin.extract({
-                use: [
-                    { loader: 'css-loader' }, { loader: "resolve-url-loader" },
-                    { loader: 'less-loader', options: { sourceMap: true } }
-                ],
-                fallback: 'vue-style-loader'
-            }),
-            stylus: ExtractTextPlugin.extract({
-                use: [
-                    { loader: 'css-loader' }, { loader: "resolve-url-loader" },
-                    { loader: 'stylus-loader', options: { sourceMap: true } }
-                ],
-                fallback: 'vue-style-loader'
-            }),
-            scss: ExtractTextPlugin.extract({
-                use: [
-                    { loader: 'css-loader' }, { loader: "resolve-url-loader" },
-                    { loader: 'sass-loader', options: { sourceMap: true } }
-                ],
-                fallback: 'vue-style-loader'
-            })
-        }
+    let vueLoaderOptions = {
+        postcss: postcssOptions(constants).plugins()
     }
 
     let _loaders = [{
@@ -95,126 +44,164 @@ export default function(constants) {
             id: "js"
         }
     }]
-    _loaders = constants.HOT ? _loaders.concat([{
-        test: /\.jsx$/,
-        exclude: [nodeModulesDir],
-        use: [
-            { loader: "react-hot-loader/webpack" },
-            { loader: "happypack/loader", options: { id: "js" } }
-        ]
-    }, {
-        test: /\.less/,
-        exclude: [nodeModulesDir],
-        use: lessLoaders
-    }, {
-        test: /\.scss/,
-        exclude: [nodeModulesDir],
-        use: sassLoaders
-    }, {
-        test: /\.styl/,
-        exclude: [nodeModulesDir],
-        use: stylusLoaders
-    }, {
-        test: /\.css/,
-        use: cssLoaders
-    }, {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        options: {
-            limit: 2500,
-            mimetype: 'application/font-woff'
+
+    if (HOT) {
+        vueLoaderOptions = {
+            ...vueLoaderOptions,
+            loaders: {
+                css: cssLoadersFactory(constants, '', true),
+                less: cssLoadersFactory(constants, 'less', true),
+                stylus: cssLoadersFactory(constants, 'stylus', true),
+                scss: cssLoadersFactory(constants, 'sass', true)
+            }
         }
-    }, {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader",
-        options: fileLoaderOptions
-    }, {
-        test: /\.(png|jpg)$/,
-        exclude: [nodeModulesDir],
-        loader: 'url-loader',
-        options: {
-            limit: 2500
-        }
-    }]) : _loaders.concat([{
+
+        _loaders = _loaders.concat([{
             test: /\.jsx$/,
             exclude: [nodeModulesDir],
-            loader: 'happypack/loader',
-            options: {
-                id: 'js'
-            }
-        }, {
-            test: /\.styl/,
-            exclude: [nodeModulesDir],
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: stylusLoaders.slice(1)
-            })
+            use: [
+                { loader: "react-hot-loader/webpack" },
+                { loader: "happypack/loader", options: { id: "js" } }
+            ]
         }, {
             test: /\.less/,
             exclude: [nodeModulesDir],
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: lessLoaders.slice(1)
-            })
+            use: lessLoaders
         }, {
             test: /\.scss/,
             exclude: [nodeModulesDir],
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: sassLoaders.slice(1)
-            })
+            use: sassLoaders
+        }, {
+            test: /\.styl/,
+            exclude: [nodeModulesDir],
+            use: stylusLoaders
         }, {
             test: /\.css/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: cssLoaders.slice(1)
-            })
-        }, {
-            test: /\.(png|jpg)$/,
-            exclude: [nodeModulesDir],
-            use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 2500,
-                        outputPath: ASSET_IMAGE_OUTPUT,
-                        hash: 'sha512',
-                        digest: 'hex',
-                        name: '[hash:8].[ext]'
-                    }
-                },
-                {
-                    loader: 'image-webpack-loader',
-                    options: {
-                        bypassOnDebug: true,
-                        // optimizationLevel: 7,
-                        // interlaced: false
-                    }
-                }
-            ]
+            use: cssLoaders
         }, {
             test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
             loader: 'url-loader',
             options: {
-                limit: 10000,
-                ...fileLoaderOptions,
-                mimetype: "application/font-woff",
-                outputPath: ASSET_FONT_OUTPUT,
-                hash: 'sha512',
-                digest: 'hex',
-                name: '[hash:8].[ext]'
+                limit: 2500,
+                mimetype: 'application/font-woff'
             }
-        },
-        {
+        }, {
             test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'file-loader',
+            loader: "file-loader",
+            options: fileLoaderOptions
+        }, {
+            test: /\.(png|jpg)$/,
+            exclude: [nodeModulesDir],
+            loader: 'url-loader',
             options: {
-                ...fileLoaderOptions,
-                outputPath: ASSET_FONT_OUTPUT,
-                hash: 'sha512',
-                digest: 'hex',
-                name: '[hash:8].[ext]'
+                limit: 2500
+            }
+        }])
+    } else {
+        vueLoaderOptions = {
+            ...vueLoaderOptions,
+            loaders: {
+                css: ExtractTextPlugin.extract({
+                    use: cssLoadersFactory(constants, '', true).slice(1),
+                    fallback: 'vue-style-loader'
+                }),
+                less: ExtractTextPlugin.extract({
+                    use: cssLoadersFactory(constants, 'less', true).slice(1),
+                    fallback: 'vue-style-loader'
+                }),
+                stylus: ExtractTextPlugin.extract({
+                    use: cssLoadersFactory(constants, 'stylus', true).slice(1),
+                    fallback: 'vue-style-loader'
+                }),
+                scss: ExtractTextPlugin.extract({
+                    use: cssLoadersFactory(constants, 'sass', true).slice(1),
+                    fallback: 'vue-style-loader'
+                })
             }
         }
-    ])
+
+        _loaders = _loaders.concat([{
+                test: /\.jsx$/,
+                exclude: [nodeModulesDir],
+                loader: 'happypack/loader',
+                options: {
+                    id: 'js'
+                }
+            }, {
+                test: /\.styl/,
+                exclude: [nodeModulesDir],
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: stylusLoaders.slice(1)
+                })
+            }, {
+                test: /\.less/,
+                exclude: [nodeModulesDir],
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: lessLoaders.slice(1)
+                })
+            }, {
+                test: /\.scss/,
+                exclude: [nodeModulesDir],
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: sassLoaders.slice(1)
+                })
+            }, {
+                test: /\.css/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: cssLoaders.slice(1)
+                })
+            }, {
+                test: /\.(png|jpg)$/,
+                exclude: [nodeModulesDir],
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 2500,
+                            outputPath: ASSET_IMAGE_OUTPUT,
+                            hash: 'sha512',
+                            digest: 'hex',
+                            name: '[hash:8].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            // optimizationLevel: 7,
+                            // interlaced: false
+                        }
+                    }
+                ]
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    ...fileLoaderOptions,
+                    mimetype: "application/font-woff",
+                    outputPath: ASSET_FONT_OUTPUT,
+                    hash: 'sha512',
+                    digest: 'hex',
+                    name: '[hash:8].[ext]'
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+                options: {
+                    ...fileLoaderOptions,
+                    outputPath: ASSET_FONT_OUTPUT,
+                    hash: 'sha512',
+                    digest: 'hex',
+                    name: '[hash:8].[ext]'
+                }
+            }
+        ])
+    }
+
     return _loaders
 }
