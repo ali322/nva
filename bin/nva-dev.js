23b31dfacecded86583358d8adc909ce048620da
alichen <ali322@gmail.com>
2017-05-12 16:13:08 +0800

    update to v0.2.36

--- bin/nva-dev.js
+++ bin/nva-dev.js
@@ -8,11 +8,5 @@ program.parse(process.argv)
 
 var port = program.port
 var browser = program.browser
-var tasks = require('nva-task')({
-    hooks:{
-        beforeDev(){
-            console.log('before')
-        }
-    }
-})
+var tasks = require('nva-task')()
 tasks.dev({ port, browser })
