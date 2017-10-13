84b5b338645a6fa31762d62399b1bd6246d51127
alichen <ali322@gmail.com>
2017-10-13 17:12:26 +0800

    upgrade v0.3.163

--- packages/nva-test-e2e/bin/index.js
+++ packages/nva-test-e2e/bin/index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 var program = require('commander')
-var test = require('../lib/index')
+var test = require('../lib/')
 let version = require('../package.json').version
 
 program.version(version)
