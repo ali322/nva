9cd281fc5efff20055e4b944e90b10e931422d5d
alichen <ali322@gmail.com>
2017-12-18 17:11:20 +0800

    upgrade v0.3.231

--- packages/nva-task/src/lib/helper.js
+++ packages/nva-task/src/lib/helper.js
@@ -6,92 +6,97 @@ import net from 'net'
 import emoji from 'node-emoji'
 
 export function lanIP () {
-  let interfaces = os.networkInterfaces()
-  let IPv4 = '127.0.0.1'
-  for (let key in interfaces) {
-    interfaces[key].forEach(function (details) {
-      if (details.family === 'IPv4' && /^en\d{1}$/.test(key) === true) {
-        IPv4 = details.address
-      }
-    })
-  }
-  return IPv4
+    let interfaces = os.networkInterfaces()
+    let IPv4 = '127.0.0.1'
+    for (let key in interfaces) {
+        interfaces[key].forEach(function (details) {
+            if (details.family === 'IPv4' && /^en\d{1}$/.test(key) === true) {
+                IPv4 = details.address
+            }
+        })
+    }
+    return IPv4
 }
 
 export function current () {
-  return new Date().toString().split(' ')[4]
+    return new Date().toString().split(' ')[4]
 }
 
 export function checkPort (port, next) {
-  let server = net.createServer(function (socket) {
-    socket.write('Echo server\r\n')
-    socket.pipe(socket)
-  })
+    let server = net.createServer(function (socket) {
+        socket.write('Echo server\r\n')
+        socket.pipe(socket)
+    })
 
-  server.listen(port, () => {
-    server.close()
-    next(true)
-  })
-  server.on('error', () => {
-    next(false)
-  })
+    server.listen(port, () => {
+        server.close()
+        next(true)
+    })
+    server.on('error', () => {
+        next(false)
+    })
 }
 
 export function bundleTime () {
-  const dateObj = new Date()
-  const year = dateObj.getFullYear()
-  const month = dateObj.getMonth() + 1
-  const date = dateObj.getDate()
-  const hour = dateObj.getHours()
-  const minute = dateObj.getMinutes()
-  return '' + year + month + date + hour + minute
+    const dateObj = new Date()
+    const year = dateObj.getFullYear()
+    const month = dateObj.getMonth() + 1
+    const date = dateObj.getDate()
+    const hour = dateObj.getHours()
+    const minute = dateObj.getMinutes()
+    return '' + year + month + date + hour + minute
 }
 
 export function relativeURL (from, to) {
-  const _url = path.relative(from, path.dirname(to)) || '.'
-  return _url + path.sep + path.basename(to)
+    const _url = path.relative(from, path.dirname(to)) || '.'
+    return _url + path.sep + path.basename(to)
 }
 
 export function checkFile (target) {
-  let stats
-  try {
-    stats = fs.statSync(path.resolve(target))
-  } catch (err) {
-    return false
-  }
-  return stats.isFile()
+    let stats
+    try {
+        stats = fs.statSync(path.resolve(target))
+    } catch (err) {
+        return false
+    }
+    return stats.isFile()
 }
 
 export function checkDir (target) {
-  let stats = fs.statSync(path.resolve(target))
-  return stats.isDirectory()
+    let stats
+    try {
+        stats = fs.statSync(path.resolve(target))
+    } catch (e) {
+        return false
+    }
+    return stats.isDirectory()
 }
 
 export function error (msg) {
-  console.log(chalk.red(msg))
-  process.exit(1)
+    console.log(chalk.red(msg))
+    process.exit(1)
 }
 
 export function emojis (key) {
-  if (os.platform() === 'darwin') {
-    return emoji.get(key)
-  } else {
-    return '>'
-  }
+    if (os.platform() === 'darwin') {
+        return emoji.get(key)
+    } else {
+        return '>'
+    }
 }
 
 export function callback (info, err, stats) {
-  if (err || stats.hasErrors()) {
-    console.log(chalk.red(err, stats))
-    return
-  }
-  console.log(
-    stats.toString({
-      version: false,
-      chunks: false,
-      modules: false,
-      colors: true
-    })
-  )
-  console.log(emojis(':ok_hand:') + '  ' + info)
+    if (err || stats.hasErrors()) {
+        console.log(chalk.red(err, stats))
+        return
+    }
+    console.log(
+        stats.toString({
+            version: false,
+            chunks: false,
+            modules: false,
+            colors: true
+        })
+    )
+    console.log(emojis(':ok_hand:') + '  ' + info)
 }
