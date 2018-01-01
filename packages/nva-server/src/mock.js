51f3f538e3ad39a1d3439752d36601ee99caeafa
alichen <ali322@gmail.com>
2018-01-01 12:27:28 +0800

    upgrade v0.3.239

--- packages/nva-server/src/mock.js
+++ packages/nva-server/src/mock.js
@@ -8,7 +8,6 @@ import { prettyError } from "./lib"
 
 import {
   find,
-  groupBy,
   values,
   reduce,
   isFunction,
@@ -22,26 +21,47 @@ export default function (app, conf) {
     let faker = require("faker/locale/en_US")
     return faker
   })
-  try {
-    let mocks = [],
-      watcher
-    if (conf.path) {
-      conf.path.split(",").forEach(v => {
-        let files = glob.sync(v)
-        files.forEach(file => {
-          let rules = require(resolve(file))
-          rules = rules.map(v => {
-            v.filename = resolve(file)
-            return v
-          })
-          mocks = mocks.concat(Array.isArray(rules) ? rules : [])
-        })
+  let mocks = {},
+    watcher
+  if (conf.path) {
+    conf.path.split(",").forEach(v => {
+      let files = glob.sync(v)
+      files.forEach(file => {
+        let rules = []
+        try {
+          rules = require(resolve(file))
+        } catch (e) {
+          console.log(prettyError(e))
+          mocks[resolve(file)] = []
+          return false
+        }
+        mocks[resolve(file)] = Array.isArray(rules) ? rules : []
       })
-      mocks = groupBy(mocks, "filename")
+    })
 
-      watcher = chokidar.watch(conf.path.split(","), { depth: 5 })
-      watcher.on("change", path => {
-        delete require.cache[resolve(path)]
+    watcher = chokidar.watch(conf.path.split(","), { depth: 5 })
+    watcher.on("change", path => {
+      delete require.cache[resolve(path)]
+      let rules = []
+      try {
+        rules = require(resolve(path))
+      } catch (e) {
+        console.log(prettyError(e))
+      }
+      if (Array.isArray(rules) === false) {
+        console.log(chalk.red('mock config must return array'))
+        return
+      }
+      mocks[path] = rules.map(v => {
+        v.filename = path
+        return v
+      })
+      if (isFunction(conf.onChange)) {
+        conf.onChange(relative(process.cwd(), path))
+      }
+    })
+    watcher.on("add", path => {
+      if (mocks[path] === undefined) {
         let rules = []
         try {
           rules = require(resolve(path))
@@ -56,75 +76,52 @@ export default function (app, conf) {
           v.filename = path
           return v
         })
-        if (isFunction(conf.onChange)) {
-          conf.onChange(relative(process.cwd(), path))
-        }
-      })
-      watcher.on("add", path => {
-        if (mocks[path] === undefined) {
-          let rules = []
-          try {
-            rules = require(resolve(path))
-          } catch (e) {
-            console.log(prettyError(e))
-          }
-          if (Array.isArray(rules) === false) {
-            console.log(chalk.red('mock config must return array'))
-            return
-          }
-          mocks[path] = rules.map(v => {
-            v.filename = path
-            return v
-          })
-          isFunction(conf.onAdd) && conf.onAdd(relative(process.cwd(), path))
-        }
-      })
-      watcher.on("unlink", path => {
-        if (mocks[path]) {
-          delete mocks[path]
-          isFunction(conf.onRemove) &&
-            conf.onRemove(relative(process.cwd(), path))
-        }
-      })
-    }
-    app.use(function (req, res, next) {
-      let rule = find(
-        reduce(values(mocks), (sum, v) => sum.concat(v), []),
-        v => {
-          let reqPath = parse(req.url).pathname
-          if (isRegExp(v.url)) {
-            return v.url.test(reqPath)
-          } else if (isString(v.url)) {
-            return v.url === reqPath
-          }
-          return false
-        }
-      )
-      if (rule && req.method.toLowerCase() === rule.method) {
-        if (
-          indexOf(
-            ["get", "post", "put", "delete", "head", "patch"],
-            rule.method
-          ) === -1
-        ) {
-          console.log(chalk.red("unsupported method"))
-        }
-        res.statusCode = 200
-        res.setHeader("content-type", "application/json;charset=utf-8")
-        let response = rule.response
-        if (rule.type === "jsf") {
-          response = jsf(response)
-        } else if (rule.type === "func") {
-          response = response(req)
-        }
-        res.end(JSON.stringify(response))
-      } else {
-        next()
+        isFunction(conf.onAdd) && conf.onAdd(relative(process.cwd(), path))
       }
     })
-  } catch (err) {
-    console.log(prettyError(err))
+    watcher.on("unlink", path => {
+      if (mocks[path]) {
+        delete mocks[path]
+        isFunction(conf.onRemove) &&
+          conf.onRemove(relative(process.cwd(), path))
+      }
+    })
   }
+  app.use(function (req, res, next) {
+    let rule = find(
+      reduce(values(mocks), (sum, v) => sum.concat(v), []),
+      v => {
+        let reqPath = parse(req.url).pathname
+        if (isRegExp(v.url)) {
+          return v.url.test(reqPath)
+        } else if (isString(v.url)) {
+          return v.url === reqPath
+        }
+        return false
+      }
+    )
+    if (rule && req.method.toLowerCase() === rule.method) {
+      if (
+        indexOf(
+          ["get", "post", "put", "delete", "head", "patch"],
+          rule.method
+        ) === -1
+      ) {
+        console.log(chalk.red("unsupported method"))
+      }
+      res.statusCode = 200
+      res.setHeader("content-type", "application/json;charset=utf-8")
+      let response = rule.response
+      if (rule.type === "jsf") {
+        response = jsf(response)
+      } else if (rule.type === "func") {
+        response = response(req)
+      }
+      res.end(JSON.stringify(response))
+    } else {
+      next()
+    }
+  })
 
   return app
 }
