7a379a1714b511ea1541386334bafca56b9e8d4f
alichen <ali322@gmail.com>
2018-01-12 11:58:49 +0800

    upgrade v0.3.248

--- packages/nva-task/src/frontend/webpack.production.js
+++ packages/nva-task/src/frontend/webpack.production.js
@@ -152,7 +152,7 @@ export default function (context, profile) {
                 rules: {
                     '.js': content =>
                         content.replace(
-                            RegExp(`\\/${staticFolder}(\\/\\s+\\.[A-Za-z]+)`,'gi'),
+                            RegExp(`\\/${staticFolder}(\\/[A-Za-z0-9-_\\.\\/"\\+]+\\.[A-Za-z]+)`,'gi'),
                             `${staticPrefix}/${staticFolder}$1`
                         ),
                     '.css': content =>
