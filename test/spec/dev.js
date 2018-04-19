0e8cd9a6c3b0993902706bee624667e8e8ee2eee
alichen <ali322@gmail.com>
2018-04-19 20:20:01 +0800

    add more tests

--- test/spec/dev.js
+++ test/spec/dev.js
@@ -2,6 +2,6 @@ import test from 'ava'
 import { devMacro } from '../fixture/macro'
 
 test.serial.cb(`frontend-boilerplate#master dev`, devMacro, 'master')
-// test.serial.cb(`frontend-boilerplate#spa dev`, devMacro, 'spa')
-// test.serial.cb(`frontend-boilerplate#vue-spa dev`, devMacro, 'vue-spa')
-// test.serial.cb(`frontend-boilerplate#vue dev`, devMacro, 'vue')
+test.serial.cb(`frontend-boilerplate#spa dev`, devMacro, 'spa')
+test.serial.cb(`frontend-boilerplate#vue-spa dev`, devMacro, 'vue-spa')
+test.serial.cb(`frontend-boilerplate#vue dev`, devMacro, 'vue')
