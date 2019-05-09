837f4e57632e26483cb6a047622448fef425d427
alichen <ali322@gmail.com>
2019-05-09 14:54:13 +0800

    upgrade env cli argument

--- bin/nva-build.js
+++ bin/nva-build.js
@@ -5,7 +5,7 @@ const checkVersion = require('../lib/check-version')
 const checkPKG = require('../lib/check-pkg')
 
 program.option('-P, --profile', 'enable profile mode', false)
-program.option('--env [value]', 'build environment', '')
+program.option('--env [value]', 'build environment name', '')
 program.option('--yarn', 'use yarn instead of npm')
 program.option('--silent', 'ignore update check')
 program.parse(process.argv)
